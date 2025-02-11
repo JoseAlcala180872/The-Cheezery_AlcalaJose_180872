8598bc87871ea734d89ed5fb6b19ccb1552bfb10
JoseAlcala180872 <jose.alcala@potros.itson.edu.mx>
2025-02-11 16:20:04 -0700

    commit segunda parte.

---
+++ TheCheezery/app/src/main/java/alcala/jose/thecheezery/ProductosActivity.kt
@@ -0,0 +1,26 @@
+package alcala.jose.thecheezery
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class ProductosActivity : AppCompatActivity() {
+
+    var coldDrinks=ArrayList<Product>()
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_productos)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
+        fun agregarProlductos(){
+            coldDrinks.add(Product("Caramel Frap", R.drawable.caramel_frap, description = "Caramel"))
+        }
+    }
+}
